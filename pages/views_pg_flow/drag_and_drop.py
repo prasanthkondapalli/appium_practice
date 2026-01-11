5aafd28d0760a040bfcfab7c616acd7a14b84f88
prasanth <prasanth@kali.kali>
2026-01-11 22:42:42 +0530

    draag and drop 1

--- pages/views_pg_flow/drag_and_drop.py
+++ pages/views_pg_flow/drag_and_drop.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+
 from pages.views_page import Views_pg
 from utils.utilities import Utils
 from appium.webdriver.common.appiumby import AppiumBy
@@ -8,10 +11,16 @@ from appium.webdriver.common.appiumby import AppiumBy
 class DragandDrop(Views_pg):
 
     drag_and_drop_ele = (AppiumBy.ANDROID_UIAUTOMATOR,'new UiSelector().text("Drag and Drop")')
-    # target_ele = (By.XPATH,'//android.view.View[@resource-id="io.appium.android.apis:id/drag_dot_1"]')
-    target_ele = (AppiumBy.ID,'io.appium.android.apis:id/drag_dot_1')
-    target2_ele = (AppiumBy.ID,'io.appium.android.apis:id/drag_dot_2')
 
+    target_ele = (By.XPATH,'//android.view.View[@resource-id="io.appium.android.apis:id/drag_dot_1"]')
+    # target_ele = (AppiumBy.ANDROID_UIAUTOMATOR,'new UiSelector().resourceId("io.appium.android.apis:id/drag_dot_1")')
+    # target_ele2 = (AppiumBy.ANDROID_UIAUTOMATOR,'new UiSelector().resourceId("io.appium.android.apis:id/drag_dot_2")')
+    target_ele2 = (By.XPATH,'//android.view.View[@resource-id="io.appium.android.apis:id/drag_dot_2"]')
+    # target_ele = (AppiumBy.ID,'io.appium.android.apis:id/drag_dot_1')
+
+
+    target2_ele = (AppiumBy.ID,'io.appium.android.apis:id/drag_dot_2')
+    ele_1 = ()
 
     def click_on_dragndrop(self):
         ut = Utils(self.driver)
@@ -19,4 +28,66 @@ class DragandDrop(Views_pg):
 
     def do_drag_drop(self):
         self.driver.execute_script("mobile: dragGesture",{"elementId":self.target_ele,"endElementId":self.target2_ele})
+
+
+    def drag_and_drop_w3c(self):
+        # Locate elements using Appium locators
+        source = self.driver.find_element(
+            AppiumBy.ANDROID_UIAUTOMATOR,
+            'new UiSelector().resourceId("io.appium.android.apis:id/drag_dot_1")'
+        )
+        print(source)
+        target = self.driver.find_element(
+            AppiumBy.ANDROID_UIAUTOMATOR,
+            'new UiSelector().resourceId("io.appium.android.apis:id/drag_dot_2")'
+        )
+        print(target)
+
+        # Get element center coordinates
+        # start_x = source.location['x'] + source.size['width'] // 2
+        # start_y = source.location['y'] + source.size['height'] // 2
+        # end_x = target.location['x'] + target.size['width'] // 2
+        # end_y = target.location['y'] + target.size['height'] // 2
+
+        # Create a touch pointer
+        # touch_input = PointerInput(PointerInput.TOUCH, "finger")
+        #
+        # # Build the action sequence
+        # actions = ActionBuilder(self.driver)
+        # actions.add_action(touch_input)
+        #
+        # # Drag-and-drop sequence
+        # touch_input.create_pointer_move(duration=0, x=start_x, y=start_y)  # move finger to start
+        # touch_input.create_pointer_down()                                   # touch down
+        # touch_input.create_pause(0.5)                                       # optional pause
+        # touch_input.create_pointer_move(duration=1000, x=end_x, y=end_y)    # move to target
+        # touch_input.create_pointer_up()                                     # lift finger
+        #
+        # # Perform the actions
+        # actions.perform()
+
+        # def drag_and_drop_w3c(self):
+            # Find elements
+        # source = self.driver.find_element(source)
+        # target = self.driver.find_element(target)
+
+        # Get center coordinates
+        start_x = source.location['x'] + source.size['width'] // 2
+        start_y = source.location['y'] + source.size['height'] // 2
+        end_x = target.location['x'] + target.size['width'] // 2
+        end_y = target.location['y'] + target.size['height'] // 2
+
+        # Create touch pointer
+        finger = PointerInput("touch", "finger")  # <-- FIX HERE
+        actions = ActionBuilder(self.driver)
+        actions.add_pointer_input('touch','finger')
+
+        # Build drag-and-drop sequence
+        finger.create_pointer_move(0, start_x, start_y)
+        finger.create_pointer_down()
+        finger.create_pause(0.5)
+        finger.create_pointer_move(1000, end_x, end_y)
+        finger.create_pointer_up(button=0)
+        actions.perform()
+
 
--- tests/test_drag_drop.py
+++ tests/test_drag_drop.py
@@ -1,8 +1,10 @@
 import time
 
 import pytest
+from requests.packages import target
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.interaction import Interaction
 
 from pages.views_pg_flow.drag_and_drop import DragandDrop
 from utils.utilities import Utils
@@ -12,17 +14,19 @@ class TestDraagdrop:
     folder = './reports/'
 
     def test_drag1(self):
-        action = ActionChains(self.driver)
         ut = Utils(self.driver)
         dd = DragandDrop(self.driver)
         dd.navigate_to_views()
         dd.click_on_dragndrop()
-        dd.do_drag_drop()
-        # ut.clicking_on_ele(dd.target_ele)
-        # # action.click_and_hold(dd.target_ele)
-        # time.sleep(2)
-        # action.drag_and_drop_by_offset(dd.target2_ele,653,536)
-        # # dd.click_on_dragndrop()
-        # ut.long_press_ele(dd.target_ele)
-        # ut.ele_drag_drop(dd.target_ele,dd.target2_ele)
-        # ut.took_screenshot(path=f'{self.folder}drag1.png')
+        action = ActionChains(self.driver)
+        dd.drag_and_drop_w3c()
+        # target_ele = self.driver.find_elements(dd.target_ele)
+        # target_ele2 = self.driver.find_elements(dd.target_ele2)
+        # action.move_to_element(dd.target_ele).click_and_hold(dd.target_ele).pause(1).move_to_element(dd.target_ele2).pause(1).release().perform()
+        # self.driver.execute_script(
+        #     "mobile: dragGesture",
+        #     {
+        #         "elementId": dd.target_ele,
+        #         "endElementId": dd.target_ele2
+        #     }
+        # )
